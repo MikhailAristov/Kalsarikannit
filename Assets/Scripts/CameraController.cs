89e9e2f09cca4bf485fd050f650d46b5967b17b6
Mikhail Aristov <mikhail.aristov@gmail.com>
2019-01-27 13:46:30 +0100

    screenshot function

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
@@ -16,5 +16,22 @@ public class CameraController : MonoBehaviour {
 		if(!Util.Approx(transform.position, Player.transform.position)) {
 			transform.position = Vector3.Lerp(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z), 5f * Time.deltaTime);
 		}
+
+		if(Input.GetKeyUp(KeyCode.Space)) {
+			takeScreenshot();
+		}
+	}
+
+	// Stores a screenshot to My Pictures
+	public void takeScreenshot() {
+		// Create directory if necessary
+		string targetDir = String.Format("{0}\\{1}", Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), Application.productName);
+		if(!Directory.Exists(targetDir)) {
+			Directory.CreateDirectory(targetDir);
+		}
+		// Take and store the screenshot
+		string filepath = String.Format("{0}\\screenshot_{1:yyyyMMddHHmmssfff}.png", targetDir, System.DateTime.Now);
+		ScreenCapture.CaptureScreenshot(filepath);
+		Debug.LogFormat("Screenshot saved to {0}.", filepath);
 	}
 }
