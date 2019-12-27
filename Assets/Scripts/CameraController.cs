69cd0885e643b10660e6977797c9564aea9076ac
Mikhail Aristov <mikhail.aristov@gmail.com>
2019-12-27 21:10:49 +0100

    smoother camera flow

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,13 +11,13 @@ public class CameraController : MonoBehaviour {
 	private const float MOUSE_SCROLL_MARGIN = 0.1f;
 	private Vector2 HalfScreenSize;
 	private Vector2 MaxDistanceToPlayer;
-	private bool FollowingPlayer;
+	private bool FocusingOnPlayer;
 
 	// Use this for initialization
 	void Start() {
 		HalfScreenSize = new Vector2(ThisCamera.orthographicSize * ThisCamera.aspect, ThisCamera.orthographicSize);
 		MaxDistanceToPlayer = HalfScreenSize / 2f;
-		FollowingPlayer = false;
+		FocusingOnPlayer = false;
 		// Confine the cursor to the game window
 		Cursor.lockState = CursorLockMode.Confined;
 	}
@@ -28,29 +28,29 @@ public class CameraController : MonoBehaviour {
 		if(Input.GetKeyUp(KeyCode.Space)) {
 			takeScreenshot();
 		}
-
-		if(FollowingPlayer) {
-			return;
-		}
-
-		// Move the camera to follow the player if necessary
-		Vector2 PlayerRelativePosition = Player.transform.position - transform.position;
-		if(Mathf.Abs(PlayerRelativePosition.x) > MaxDistanceToPlayer.x || Mathf.Abs(PlayerRelativePosition.y) > MaxDistanceToPlayer.y) {
-			transform.position = Vector3.Lerp(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z), 0.5f * Time.deltaTime);
-		}
-
-		// Follow the mouse cursor
-		Vector3 MouseRelativePosition = ThisCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)) - transform.position;
-		if((MouseRelativePosition.x > HalfScreenSize.x - MOUSE_SCROLL_MARGIN && PlayerRelativePosition.x > -MaxDistanceToPlayer.x + MOUSE_SCROLL_MARGIN)
-			|| (MouseRelativePosition.x < -HalfScreenSize.x + MOUSE_SCROLL_MARGIN && PlayerRelativePosition.x < MaxDistanceToPlayer.x - MOUSE_SCROLL_MARGIN)
-			|| (MouseRelativePosition.y > HalfScreenSize.y - MOUSE_SCROLL_MARGIN && PlayerRelativePosition.y > -MaxDistanceToPlayer.y + MOUSE_SCROLL_MARGIN)
-			|| (MouseRelativePosition.y < -HalfScreenSize.y + MOUSE_SCROLL_MARGIN && PlayerRelativePosition.y < MaxDistanceToPlayer.y - MOUSE_SCROLL_MARGIN)) {
-			transform.Translate(MouseRelativePosition * Time.deltaTime);
-		}
 	}
 
-	// Stores a screenshot to My Pictures
-	public void takeScreenshot() {
+    private void LateUpdate() {
+        if(!FocusingOnPlayer) {
+            // Move the camera to follow the player if necessary
+            Vector2 PlayerRelativePosition = Player.transform.position - transform.position;
+            if(Mathf.Abs(PlayerRelativePosition.x) > MaxDistanceToPlayer.x || Mathf.Abs(PlayerRelativePosition.y) > MaxDistanceToPlayer.y) {
+                transform.position = Vector3.Lerp(transform.position, new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z), 0.5f * Time.deltaTime);
+            }
+
+            // Follow the mouse cursor
+            Vector3 MouseRelativePosition = ThisCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)) - transform.position;
+            if((MouseRelativePosition.x > HalfScreenSize.x - MOUSE_SCROLL_MARGIN && PlayerRelativePosition.x > -MaxDistanceToPlayer.x + MOUSE_SCROLL_MARGIN)
+                || (MouseRelativePosition.x < -HalfScreenSize.x + MOUSE_SCROLL_MARGIN && PlayerRelativePosition.x < MaxDistanceToPlayer.x - MOUSE_SCROLL_MARGIN)
+                || (MouseRelativePosition.y > HalfScreenSize.y - MOUSE_SCROLL_MARGIN && PlayerRelativePosition.y > -MaxDistanceToPlayer.y + MOUSE_SCROLL_MARGIN)
+                || (MouseRelativePosition.y < -HalfScreenSize.y + MOUSE_SCROLL_MARGIN && PlayerRelativePosition.y < MaxDistanceToPlayer.y - MOUSE_SCROLL_MARGIN)) {
+                transform.Translate(MouseRelativePosition * Time.deltaTime);
+            }
+        }
+    }
+
+    // Stores a screenshot to My Pictures
+    public void takeScreenshot() {
 		// Create directory if necessary
 		string targetDir = String.Format("{0}\\{1}", Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), Application.productName);
 		if(!Directory.Exists(targetDir)) {
@@ -63,7 +63,7 @@ public class CameraController : MonoBehaviour {
 	}
 
 	public void FocusOnPlayer() {
-		FollowingPlayer = true;
+		FocusingOnPlayer = true;
 		StartCoroutine(MoveOverPlayerToken());
 	}
 
